238e3fa2a80556a45a478001a06c55e91bbaa567
Alex Kozlov <alexssource@gmail.com>
2012-04-20 01:10:24 +0000

    Test deploy

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <?php include_http_metas() ?>
+    <?php include_http_metas(); ?>
     <?php include_metas() ?>
       <title><?php include_slot('title', '1CRegion.ru - биржа фриланса для 1С'); ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
