54b4857348709a7d0e5d52d264ccbfb608413f06
Alex Kozlov <alexssource@gmail.com>
2012-12-08 02:35:33 +0300

    Created a portfolio

--- apps/frontend/modules/portfolio/templates/showSuccess.php
+++ apps/frontend/modules/portfolio/templates/showSuccess.php
@@ -1,67 +1,69 @@
-<table>
-  <tbody>
-    <tr>
-      <th>Id:</th>
-      <td><?php echo $portfolio_work->getId() ?></td>
-    </tr>
-    <tr>
-      <th>User:</th>
-      <td><?php echo $portfolio_work->getUserId() ?></td>
-    </tr>
-    <tr>
-      <th>Name:</th>
-      <td><?php echo $portfolio_work->getName() ?></td>
-    </tr>
-    <tr>
-      <th>Description:</th>
-      <td><?php echo $portfolio_work->getDescription() ?></td>
-    </tr>
-    <tr>
-      <th>Category:</th>
-      <td><?php echo $portfolio_work->getCategoryId() ?></td>
-    </tr>
-    <tr>
-      <th>Image:</th>
-      <td><?php echo $portfolio_work->getImage() ?></td>
-    </tr>
-    <tr>
-      <th>Link:</th>
-      <td><?php echo $portfolio_work->getLink() ?></td>
-    </tr>
-    <tr>
-      <th>Cost:</th>
-      <td><?php echo $portfolio_work->getCost() ?></td>
-    </tr>
-    <tr>
-      <th>Cost currency:</th>
-      <td><?php echo $portfolio_work->getCostCurrencyId() ?></td>
-    </tr>
-    <tr>
-      <th>Term:</th>
-      <td><?php echo $portfolio_work->getTerm() ?></td>
-    </tr>
-    <tr>
-      <th>Term options:</th>
-      <td><?php echo $portfolio_work->getTermOptions() ?></td>
-    </tr>
-    <tr>
-      <th>File src:</th>
-      <td><?php echo $portfolio_work->getFileSrc() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $portfolio_work->getCreatedAt() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $portfolio_work->getUpdatedAt() ?></td>
-    </tr>
-  </tbody>
-</table>
+<?php use_helper("Text"); ?>
+<?php use_stylesheet('portfolio.css'); ?>
 
-<hr />
+<h1 class="portfolio_title">Портфолио пользователя <?php echo $portfolio_work->User; ?></h1>
+<h3 class="portfolio_work_name"><?php echo $portfolio_work->getName(); ?></h3>
 
-<?php if($portfolio_work->User == $sf_user->getGuardUser()): ?>
-    <a href="<?php echo url_for('portfolio_edit', $portfolio_work); ?>">Edit</a>&nbsp;
-<?php endif; ?>
-<a href="<?php echo url_for('portfolio', $portfolio_work->User); ?>">List</a>
+<div id="portfolio_show">
+    <?php if($portfolio_work->getImage()): ?>
+        <img class="img_work" align="left" src="<?php echo '/uploads/UserFiles/'.$portfolio_work->getImage(); ?>" alt="<?php echo $portfolio_work->getName(); ?>" />
+    <?php else: ?>
+        <img class="img_work" align="left" src="<?php echo sfConfig::get('app_default_workimage_path'); ?>" alt="<?php echo $portfolio_work->getName(); ?>" />
+    <?php endif; ?>
+    <div id="portfolio_work_info">
+        <table class="table_work_options">
+            <tbody>
+                <tr>
+                    <td class="td_work_options">Категория:</td>
+                    <td class="td_work_value"><?php echo $portfolio_work->Category; ?></td>
+                </tr>
+                <?php if($portfolio_work->getCost()): ?>
+                    <tr>
+                        <td class="td_work_options">Стоимость выполенения:</td>
+                        <td class="td_work_value"><?php echo $portfolio_work->getCost(); ?>&nbsp;<?php echo $portfolio_work->CostCurrency; ?></td>
+                    </tr>
+                <?php endif; ?>
+                <?php if($portfolio_work->getTerm()): ?>
+                    <tr>
+                        <td class="td_work_options">Срок выполенения:</td>
+                        <td class="td_work_value"><?php echo $portfolio_work->getTerm(); ?>&nbsp;<?php echo $portfolio_work->getTermOptions(); ?></td>
+                    </tr>
+                <?php endif; ?>
+                <?php if($portfolio_work->getLink()): ?>
+                    <tr>
+                        <td class="td_work_options">Ссылка:</td>
+                        <td class="td_work_value"><a class="link_work" href="<?php echo $portfolio_work->getLink(); ?>"><?php echo $portfolio_work->getLink(); ?></a></td>
+                    </tr>
+                <?php endif; ?>
+                <?php if($portfolio_work->getFileSrc()): ?>
+                    <tr>
+                        <td class="td_work_options">Вложения:</td>
+                        <td class="td_work_value"><a class="link_work" href="<?php echo '/uploads/UserFiles/'.$portfolio_work->getFileSrc(); ?>"><?php echo $portfolio_work->getFileSrc(); ?></a></td>
+                    </tr>
+                <?php endif; ?>
+                <tr>
+                    <td class="td_work_options">Добавлено:</td>
+                    <td class="td_work_value"><?php echo $portfolio_work->getCreatedAt(); ?></td>
+                </tr>
+                <tr>
+                    <td class="td_work_options">Обновлено:</td>
+                    <td class="td_work_value"><?php echo $portfolio_work->getUpdatedAt(); ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <br clear="left" />
+    
+    <div id="portfolio_description">
+        <?php echo simple_format_text($portfolio_work->getDescription()); ?>
+    </div>
+</div>
+
+<div id="portfolio_navigation">
+    <?php if($portfolio_work->User == $sf_user->getGuardUser()): ?>
+        <a class="link_portfolio_nav" href="<?php echo url_for('portfolio_edit', $portfolio_work); ?>">Редактировать</a>&nbsp;
+        <a class="link_portfolio_nav" href="<?php echo url_for('portfolio_delete', $portfolio_work); ?>">Удалить</a>
+    <?php endif; ?>
+    <a class="link_portfolio_nav" href="<?php echo url_for('portfolio', $portfolio_work->User); ?>">Назад к списку работ</a>
+</div>
--- lib/form/doctrine/PortfolioWorkForm.class.php
+++ lib/form/doctrine/PortfolioWorkForm.class.php
@@ -13,5 +13,43 @@ class PortfolioWorkForm extends BasePortfolioWorkForm
   public function configure()
   {
       unset($this['user_id'], $this['created_at'], $this['updated_at']);
+      
+      $this->widgetSchema['image'] = new sfWidgetFormInputFile();
+      $this->validatorSchema['image'] = new sfValidatorFile(array(
+          'required'    => false,
+          'mime_types'  => 'web_images',
+          'path'        => sfConfig::get('sf_upload_dir').'/UserFiles',
+      ));
+      
+      $this->widgetSchema['file_src'] = new sfWidgetFormInputFile();
+      $this->validatorSchema['file_src'] = new sfValidatorFile(array(
+          'required'    => false,
+          'path'        => sfConfig::get('sf_upload_dir').'/UserFiles',
+      ));
+      
+      $this->getWidgetSchema()->setLabels(array(
+              'name'        => 'Название',
+              'description' => 'Описание',
+              'category_id' => 'Категория',
+              'image'       => 'Изображение',
+              'link'        => 'Адрес проекта',
+              'cost'        => 'Стоимость',
+              'term'        => 'Сроки',
+              'file_src'    => 'Вложить файл'
+      ))
+      ->setHelps(array(
+              'name'        => 'Введите название работы',
+              'description' => 'Введите описание работы',
+              'category_id' => 'Выберити из списка категорию',
+              'image'       => 'Вы можете загрузить изображение',
+              'link'        => 'Адрес сайта разработанного проекта',
+              'cost'        => 'Стоимость выполнения работы',
+              'term'        => 'Срок, за который работа была выполнена',
+              'file_src'    => 'Можно загрузить файл работы'
+      ));
+  }
+  
+  public function getStylesheets() {
+      return array('project_form.css' => null, 'portfolio_form.css' => null);
   }
 }
